c79b0537efe71a6f6cc3337a4eca4b5bb40dd3ff
amburkee <allie_burkeen@msn.com>
2021-01-12 14:59:40 -0500

    Fixing errors

--- Project_0/src/main/scala/Cli.scala
+++ Project_0/src/main/scala/Cli.scala
@@ -1,21 +1,16 @@
 import scala.io.StdIn
 import scala.util.matching.Regex 
 import java.io.FileNotFoundException
+import scala.io.Source
 
 class Cli{
-    var location = "Beach"
+    var location : String = "Beach"
     var inventory = new Array[String](4)
-    var shedStat = new Array[Int](4)
+    var shedStat = new Array[String](4)
 
     val commandArg: Regex = "(\\w+)\\s*(.*)".r
 
-    def printWelcome() : Unit = {
-        println("You wake up on an empty beach. Looking around there is no one to be found. It looks like no one has ever been here before.")
-        println("What will you do?")
-    }
-
     def printOptions(): Unit ={
-        println("Actions: ")
         // print CSV File, has action words on it...
         val filename = "src/main/scala/csvFile.csv"
         for(line <- Source.fromFile(filename).getLines){
@@ -28,55 +23,61 @@ class Cli{
     }
 
     def menu(): Unit = {
-        printWelcome()
         var continueMenuLoop = true
         while (continueMenuLoop) {
-            printOptions()
+            printOptions
             val input = StdIn.readLine()
 
             input match {
                 case commandArg(cmd, arg) if cmd.equalsIgnoreCase("Look") => {
-                    lookArg()
+                    lookArg
                 }
                 case commandArg(cmd, arg) if cmd.equalsIgnoreCase("Move") => {
-                   moveArg()
-
+                    if (arg == location){
+                        locationNotAvailable
+                    }else{
+                        moveArg
+                    } 
                 }
                 case commandArg(cmd, arg) if cmd.equalsIgnoreCase("Use") => {
-                    shedStatus()
+                    if (shedStat.contains(arg)){
+
+                    }
+                    shedStatus
                     
                 }
             }
         }
 
     }
-    class lookArg{
+
+    def lookArg{
         //look around area
         //need to search areas for items,
         //if items were found, there is nothing to pickup there 
         //items will be stored in inventory, if item is in inventory, area is empty
-        if (location = "beach"){
+        if (location == "beach"){
             println("An empty beach. Looks like no one has been here before.")
         }
-        if (location = "Forest"){
-            if (inventory(0) = "Bolt Cutters"){
+        if (location == "Forest"){
+            if (inventory(0) == "Bolt Cutters"){
                 println("An overgrown forest. There is so much greenery here, you can't even see the sky or ground.")
             }else{
-                inventory(0) = "Bolt Cutters"
+                inventory(0) == "Bolt Cutters"
                 println("You've found Bolt Cutters!")
             }
         }
-        if (location = "Shed"){
-            if (inventory(1) = "Net"){
+        if (location == "Shed"){
+            if (inventory(1) == "Net"){
                 println("It is an old shed... I wonder who built it?")
             }else{
-                inventory(1) = "Net"
+                inventory(1) == "Net"
                 println("You've found a net!")
             }
 
         }
-        if (location = "Waterfall"){
-            if (inventory(2) = "Crowbar"){
+        if (location == "Waterfall"){
+            if (inventory(2) == "Crowbar"){
                 println("A not so big waterfall but a waterfall none of the less. It is quite peaceful here.")
             }else{
                 inventory(2) = "Crowbar"
@@ -84,8 +85,8 @@ class Cli{
             }
 
         }
-        if (location = "Cliff"){
-            if (inventory(3) = "Key"){
+        if (location == "Cliff"){
+            if (inventory(3) == "Key"){
                 println("That's a long way down... Better keep my distance.")
             }else{
                 inventory(3) = "Key"
@@ -93,49 +94,35 @@ class Cli{
             }
 
         }else{
-            locationError()
+            locationError
         }
     }
-    class moveArg{
+    def moveArg{
         //move to another area
         //areas: Beach (start), Forest, Shed, Waterfall, Cliff
-        if (location = "beach"){
-
-        }
-        if (location = "Forest"){
-
-        }
-        if (location = "Shed"){
-
-        }
-        if (location = "Waterfall"){
-
-        }
-        if (location = "Cliff"){
-
-        }else{
-            locationError()
-        }
+        
     }
 
-    class useArg{
+    def useArg{
         println("What would you like to use?")
         println(inventory)
 
     }
 
-    class shedStatus{
-        if (shedStat.contains(arg)){
-            
-        }
+    def shedStatus{
+        
 
     }
 
 
 
 
-    class locationError{
+    def locationError{
         println("Location not found... Please select an area")
+        menu()
+    }
+    def locationNotAvailable{
+        println(s"You are already at $location. Please select another location.")
         menu()
     }
 }
--- Project_0/src/main/scala/Main.scala
+++ Project_0/src/main/scala/Main.scala
@@ -1,11 +1,17 @@
+import scala.io.StdIn
+import scala.util.matching.Regex 
+import java.io.FileNotFoundException
 import scala.io.Source
 
 object Main extends App {
-  
-  //......
+  val newGame = new Cli()
 
-  println("You have done it! You opened the Shed and found a lot of material inside. You go and take it all to the beach to put it " +
-    "together.")
+  println("You wake up on an empty beach. Looking around there is no one to be found. It looks like no one has ever been here before.")
+  println("What will you do?")
+
+  newGame.menu()
+  
+  println("You have done it! You opened the Shed and found a lot of materials inside. You take it all to the beach to put it together.")
   println("Congrats you have escaped the island! Would you like to play again? Y/N")
 
 }
